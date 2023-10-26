dbee3a757fd89d350b5f6cea6e5c44afa697d027
John Heger <heger7@gmail.com>
2023-10-26 14:10:46 -0500

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+
+function submitData(name, email) {
+    const userInfo = {
+        name: name,
+        email: email,
+    };
+    const configurationUser = {
+        method: "POST",
+        headers: {
+            "Content-Type": 'application/json',
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(userInfo),
+    }
+
+    return fetch("http://localhost:3000/users", configurationUser)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(user){
+        console.log(user.id);
+        let body = document.querySelector('body')
+        body.append(user.id);
+    })
+    .catch(function(error) {
+        alert("Bad info!")
+        let body = document.querySelector('body')
+        body.append(error.message);
+    })
+}
